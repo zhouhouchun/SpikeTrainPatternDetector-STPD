5fe25beac2d3bfeb65812c974abdb9f03bc3f10e
Zhou Houchun <126965130+zhouhouchun@users.noreply.github.com>
2026-09-02 03:11:59 +0700

    CHORE: split macOS app into standalone repository

--- macos/SpikeTrainPatternDetectorMac/Sources/SpikeTrainPatternDetectorMac/Support/STPDAppTheme.swift
+++
@@ -1,7 +0,0 @@
-import AppKit
-import SwiftUI
-
-enum STPDAppTheme {
-    static let accent = Color(red: 0x45 / 255.0, green: 0x8B / 255.0, blue: 0x6E / 255.0)
-    static let nsAccent = NSColor(red: 0x45 / 255.0, green: 0x8B / 255.0, blue: 0x6E / 255.0, alpha: 1)
-}
